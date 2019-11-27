a0bccc1bc795f96b319b47b0840cbc72cb32fe4c
kenrjones <krjones502@gmail.com>
2019-11-27 13:01:38 -0500

    added forum discriptions and created README.md

--- src/Slash.js
+++ src/Slash.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Container, Card, Row, Col, Button, } from 'react-bootstrap';
 import axios from 'axios';
 import { Link } from "react-router-dom"
-import AnimeIMG from "./Images/20f6e3b764e4f26231ff0cc5b8b68865.jpg" 
+import AnimeIMG from "./Images/20f6e3b764e4f26231ff0cc5b8b68865.jpg"
 import MusicIMG from "./Images/37821.jpg"
 import OracleIMG from "./Images/fullsizeoutput_6be0.jpg"
 import SportsIMG from "./Images/nyKIn6ZJ_400x400.jpg"
@@ -52,7 +52,7 @@ class Home extends React.Component {
               <Card.Title>Anime </Card.Title>
               <Card.Img className="forum-img" variant="top" src={AnimeIMG} />
               <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                Where fans from around the world can gather to discuss anime and Japanese culture! All anime fans are welcome.
               </Card.Text>
               <Link to="/anime" className="btn btn-danger" role="button" aria-pressed="true">Anime</Link>
             </Card.Body>
@@ -63,7 +63,7 @@ class Home extends React.Component {
               <Card.Title>Music </Card.Title>
               <Card.Img className="forum-img" variant="top" src={MusicIMG} />
               <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                The place to discuss music of all genres! Be it todays top headlines or sharing your original content, this is the place to be.
               </Card.Text>
               <Link to="/music" className="btn btn-danger" role="button" aria-pressed="true">Music</Link>
             </Card.Body>
@@ -75,7 +75,7 @@ class Home extends React.Component {
               <Card.Title>Oracle Cards </Card.Title>
               <Card.Img className="forum-img" variant="top" src={OracleIMG} />
               <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                Here we discuss Oracle decks, ivination decks, meditation cards, and cartomantic or non-Tarot decks of all kinds.
               </Card.Text>
               <Link to="/oraclecards" className="btn btn-danger" role="button" aria-pressed="true">Oracal Cards</Link>
             </Card.Body>
@@ -87,7 +87,7 @@ class Home extends React.Component {
               <Card.Title>Sports </Card.Title>
               <Card.Img className="forum-img" variant="top" src={SportsIMG} />
               <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                The ultimate sports fan community where we share and dicuss current news, fantisay teams and video games.
               </Card.Text>
               <Link to="/sports" className="btn btn-danger" role="button" aria-pressed="true">Sports</Link>
             </Card.Body>
